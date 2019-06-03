ec16870a5d2e07d73e5d9a4011e724660ade3280
adamenkost <nikitaadamenko@yandex.ru>
2019-06-03 20:56:38 +0300

    Update script.js

--- travell/js/script.js
+++ travell/js/script.js
@@ -25,36 +25,8 @@ $('.testimonials__items').slick({
     dots: true,
     infinite: true,
     speed: 300,
-    slidesToShow: 2,
+    slidesToShow: 1,
     slidesToScroll: 2,
     loop: true,
     arrows: true,
-    responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                arrows: false
-            }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-    ]
 });
